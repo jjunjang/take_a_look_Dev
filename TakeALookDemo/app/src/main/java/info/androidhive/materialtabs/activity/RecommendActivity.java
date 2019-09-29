f167d408d2c9cc022092060e96551c828331d9fd
ollehLeader <50910363+ollehLeader@users.noreply.github.com>
2019-09-29 23:43:20 +0900

    recommend
    새로고침 시스템은 구현(기능 미구현)
    
    이미지클릭 미구현

--- TakeALookDemo/app/src/main/java/info/androidhive/materialtabs/activity/RecommendActivity.java
+++ TakeALookDemo/app/src/main/java/info/androidhive/materialtabs/activity/RecommendActivity.java
@@ -1,6 +1,7 @@
 package info.androidhive.materialtabs.activity;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -8,6 +9,7 @@ import info.androidhive.materialtabs.R;
 
 public class RecommendActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    SwipeRefreshLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +18,22 @@ public class RecommendActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        layout = (SwipeRefreshLayout) findViewById(R.id.layout);
+        layout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                //새로고침 작업 실행...
+
+                layout.setRefreshing(false);
+
+            }
+        });
+
+
+
     }
+
+
 }

69a451255cff17a0658d597d47f4854407be4a35
JJunJang <JJunJang1280@naver.com>
2019-11-25 10:35:02 +0900

    del
    del

--- MusicPlayer-master/app/src/main/java/com/massivcode/androidmusicplayer/events/Event.java
+++
@@ -1,7 +0,0 @@
-package com.massivcode.androidmusicplayer.events;
-
-/**
- * Created by Ray Choe on 2015-11-24.
- */
-public interface Event {
-}
--- MusicPlayer-master/app/src/main/java/com/massivcode/androidmusicplayer/events/ReloadPlaylist.java
+++
@@ -1,7 +0,0 @@
-package com.massivcode.androidmusicplayer.events;
-
-/**
- * Created by Ray Choe on 2015-12-02.
- */
-public class ReloadPlaylist implements Event {
-}
